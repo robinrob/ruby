f5e485b858f69a6c6ec52f675b729806f9372564
Robin Smith <robin.smith@cloudreach.co.uk>
2014-09-08 16:23:28 -0700

    Auto-update.

---
+++ practice/load.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+load 'loaded.rb'
+
+loaded = Loaded.new()
+
+puts "loaded.field1: #{loaded.field1}"
---
+++ practice/loaded.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+class Loaded
+  attr_accessor :field1
+  
+  def initialize()
+    @field1 = "Hello Robin Smith"
+  end
+end
