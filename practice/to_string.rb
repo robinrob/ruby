910696669cb42b3b8757cf817212109492d996d7
Robin Smith <robin@mrrobinsmith.com>
2014-09-14 19:28:43 -0700

    Auto-update.

---
+++ practice/to_string.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH << '.'
+
+require 'lib/log.rb'
+
+
+class Helium
+  def to_s
+    "He"
+  end
+
+  def to_str
+    "helium"
+  end
+end
+
+e = Helium.new
+print "Element is "
+puts e                    # Element is He
+puts "Element is " + e    # Element is helium
+puts "Element is #{e}"    # Element is He
